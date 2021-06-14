da1069ed612c126c3a5201c44a1a53da6085499c
Talgat <otli4nik123@gmail.com>
2021-06-14 09:11:02 +0600

    fix

--- RestaurantOrderSys-last-master/RestaurantOrderSys-master/orders/static/orders/main.js
+++ RestaurantOrderSys-last-master/RestaurantOrderSys-master/orders/static/orders/main.js
@@ -68,16 +68,23 @@ function add_to_cart(info){
   //info will be the stuff displayed in the reciept
   // item description as well as teh price
   display_notif("add to cart", info);
-  var cart_retrieved = localStorage.getItem("cart")
-  if (cart_retrieved === null){
-    //make a new cart
-    var cart = [info];
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }else{
-    var cart = JSON.parse(cart_retrieved);
-    cart.push(info)
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }
+
+    /*localStorage.setItem('cart','{ "cart" : [] }')*/
+  var cart_retrieved = JSON.parse(localStorage.getItem("cart"))
+    cart_retrieved.cart.push(info)
+    localStorage.setItem('cart', JSON.stringify(cart_retrieved))
+    console.log(cart_retrieved)
+
+
+  // if (cart_retrieved === null){
+  //   //make a new cart
+  //   var cart = [info];
+  //   localStorage.setItem('cart', JSON.stringify(cart));
+  // }else{
+  //   var cart = JSON.parse(cart_retrieved);
+  //   cart.push(info)
+  //   localStorage.setItem('cart', JSON.stringify(cart));
+  // }
 
 
 }
@@ -126,23 +133,24 @@ function display_notif(type, info="No info provided"){
 }
 
 function load_cart(){
+    var cart_retrieved = JSON.parse(localStorage.getItem("cart"))
+
   var table = document.getElementById('cart_body');
   table.innerHTML = ""; //clear the table
   document.getElementById('cart_heading').innerHTML = "To remove an item from cart, simply click it..."
-  var cart = JSON.parse(localStorage.getItem("cart"));
   var total = 0;
-  if (cart !== null && cart.length > 0){
-    for (var i = 0; i < cart.length; i++) {
+  if (cart_retrieved !== null && cart_retrieved.cart.length > 0){
+    for (var i = 0; i < cart_retrieved.cart.length; i++) {
 
       var row = table.insertRow(-1);
       var item_number = row.insertCell(0);
       var item_description = row.insertCell(1);
       var item_price = row.insertCell(2);
       item_number.innerHTML = String(i+1);
-      item_description.innerHTML = cart[i].item_description;
-      item_price.innerHTML = "£"+cart[i].price;
+      item_description.innerHTML = cart_retrieved.cart[i].item_description;
+      item_price.innerHTML = "£"+cart_retrieved.cart[i].price;
 
-      total += cart[i].price
+      total += cart_retrieved.cart[i].price
     }
     total = Math.round(total * 100) / 100
     localStorage.setItem('total_price', total);
@@ -250,15 +258,18 @@ function clear_cart(){
 
 function checkout(){
   //this is the function that will be run when the user wants to checkout
-  var cart = localStorage.getItem("cart")
-  var price_of_cart = localStorage.getItem("total_price")
+    var cart_retrieved = JSON.parse(localStorage.getItem("cart"))
+    let total=0
+    for(let i=0; i<cart_retrieved.cart.length;i++) {
+        total+=cart_retrieved.cart[i].price
+    }
   var csrftoken = getCookie('csrftoken');
 
   console.log("Checkout was clicked so we now send it to the server!") // sanity check
   $.ajax({
       url : "/checkout" , // the endpoint
       type : "POST", // http method
-      data : { cart : cart, price_of_cart: price_of_cart, csrfmiddlewaretoken: csrftoken }, // data sent with the post request
+      data : { cart : JSON.stringify(cart_retrieved.cart)    , price_of_cart: total, csrfmiddlewaretoken: csrftoken }, // data sent with the post request
 
       // handle a successful response
       success : function(json) {
@@ -277,17 +288,20 @@ function checkout(){
 }
 
 function logout(){
-  var current_cart = localStorage.getItem("cart")
+
+    localStorage.setItem('cart','{ "cart" : [] }')//delete and again add
+    var cart_retrieved=JSON.parse(localStorage.getItem('cart'))
   var csrftoken = getCookie('csrftoken');
   $.ajax({
       url : "/save_cart" , // the endpoint
       type : "POST", // http method
-      data : { cart : current_cart, csrfmiddlewaretoken: csrftoken }, // data sent with the post request
+      data : { cart :'{ "cart": [] }' , csrfmiddlewaretoken: csrftoken }, // data sent with the post request
 
       // handle a successful response
       success : function(json) {
-        //clear the local storage
-        localStorage.removeItem("cart"); //Clear the cart
+        // //clear the local storage
+        // localStorage.setItem('cart','{ "cart" : [] }')
+        // localStorage.removeItem("cart"); //Clear the cart
         localStorage.setItem('cart_retrieved', false);
         window.location.href = "/logout";
       },
